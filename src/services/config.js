de24513d9a0cb5722ac313a98cd4961b85bd7b88
jocarrd <jocarrd@unirioja.es>
2022-07-05 10:32:32 +0200

    despliege en vercel

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = "https://weather-app-jocarrd.vercel.app/";
+
 const API = axios.create({
-  baseURL: window.location.origin,
+  baseURL: BASE_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
